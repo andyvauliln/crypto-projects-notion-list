7438bb67528352f7ca9da9c6ebe6706030bd7c80
Andrei Vaulin <johnnypitt.ind@gmail.com>
2022-12-14 16:15:22 +0800

    added siter scraper

--- src/index.js
+++ src/index.js
@@ -5,29 +5,30 @@ import { getTokens } from './coinmarketcup';
 import {
   getGihubRepos,
   getMarkdownFromRepo,
-  getPackageJsonFromRepo,
+  getPackageJsonFromRepo
 } from './github';
 import LoggerInstance from './loging';
 import {
   addRepsoitoryToNotion,
-  addTokenToNotion,
-  getLastToken,
-  getNotionTokensWithSourceCode,
+  addTokenToNotion, getLastContentDowloadedTokenId, getLastRepositoryTokenId, getLastTokenId, getNotionTokensWithSites, getNotionTokensWithSourceCode
 } from './notion';
+import { getSiteData } from './siteScraper';
 
 // ******************** DOWNLOAD TOKENS FROM COINMARKETCAP TO NOTION ***********************
 
-const requestLimitPerDay = 333 - 90;
+
 async function migrateFromCoinmarketCap() {
+  const requestLimitPerDay = 333;
+  const limit = 99;
   try {
     await LoggerInstance.cleanLogs();
     for (let d = 0; d < requestLimitPerDay; d++) {
       await LoggerInstance.logInfo('NEW CYCLE: ' + d);
-      const lastTokenId = await getLastToken();
+      const lastTokenId = await getLastTokenId();
       await LoggerInstance.logInfo(
         'migrateFromCoinmarketCap LAST TOKEN ID: ' + lastTokenId
       );
-      const tokens = await getTokens(lastTokenId + 1, lastTokenId + 98);
+      const tokens = await getTokens(lastTokenId + 1, lastTokenId + limit);
       for (let i = 0; i < tokens.length; i++) {
         await LoggerInstance.logInfo(
           'migrateFromCoinmarketCap START ADDING TOKEN TO NOTION: ' +
@@ -46,23 +47,28 @@ async function migrateFromCoinmarketCap() {
     await LoggerInstance.logError(
       `migrateFromCoinmarketCap \n\n ${error.message}, \n\n ${error.stack}`
     );
+    throw new Error("TOKEN NOT ADDED");
   }
 }
 
 // ******************** DOWNLOAD REPOS FROM GITHUB TO NOTION ***********************
 
-const amountRequest = 1;
-const limit = 50;
+
 async function downloadProjectRepositories() {
+  const amountRequest = 1;
+  const limit = 100;
   try {
     await LoggerInstance.cleanLogs();
+    const lastTokenId = await getLastRepositoryTokenId();
+    await LoggerInstance.logInfo(
+      'downloadProjectRepositories: LAST TOKEN ID: ' + lastTokenId
+    );
     for (let i = 0; i < amountRequest; i++) {
-      await LoggerInstance.logInfo(
-        'downloadProjectRepositories NEW CYCLE: ' +
-          ` offset: ${i * limit || 1} limit: ${limit}`
-      );
-      const tokens = await getNotionTokensWithSourceCode(i * limit || 1, limit);
 
+      const tokens = await getNotionTokensWithSourceCode(lastTokenId + 1, lastTokenId + limit);
+      await LoggerInstance.logInfo(
+        `getNotionTokensWithSourceCode: Length: ${tokens.length} \n\n ${JSON.stringify(tokens)}`
+      );
       for (let j = 0; j < tokens.length; j++) {
         await LoggerInstance.logInfo(
           'downloadProjectRepositories PROCESSING: ' +
@@ -95,13 +101,20 @@ async function downloadProjectRepositories() {
     }
     await LoggerInstance.logInfo('downloadProjectRepositories DONE');
   } catch (error) {
-    console.log(error);
     await LoggerInstance.logError(
       `downloadProjectRepositories \n\n ${error.message} \n\n ${error.stack}`
     );
+    throw new Error("TOKEN NOT ADDED");
   }
 }
 
+
+// ***************** UTILS ***************
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
 async function generateJsonExtentationFile() {
   let tempObj = {};
   Object.entries(languages2).forEach(([key, value]) => {
@@ -114,18 +127,76 @@ async function generateJsonExtentationFile() {
   );
 }
 
-// ***************** UTILS ***************
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
+async function scrapSiteData() {
+  const requestsAmount = 1;
+  const limit = 100;
+  try {
+    await LoggerInstance.cleanLogs();
+    const lastTokenId = await getLastContentDowloadedTokenId();
+    await LoggerInstance.logInfo(
+      'downloadProjectRepositories: LAST TOKEN ID: ' + lastTokenId
+    );
+    for (let i = 0; i < scrapAmountRequest2; i++) {
+      await LoggerInstance.logInfo(
+        'scrapSiteData NEW CYCLE: ' +
+        ` offset: ${i * limit2 || 1} limit: ${limit2}`
+      );
+      const tokens = await getNotionTokensWithSites(lastTokenId + 1, lastTokenId + 100);
+      let resp = {};
+      for (let j = 0; j < tokens.length; j++) {
+        await LoggerInstance.logInfo(
+          'scrapSiteData PROCESSING: ' +
+          `ID: ${tokens[j].properties.Id.number} TOKEN: ${tokens[j].properties.Name.title[0].text.content}`
+        );
+        const websites =
+          tokens[j].properties.Website.rich_text[0].plain_text.split(',');
+
+        await asyncForEach(websites, async (link) => {
+          await LoggerInstance.logInfo(
+            'getInformationFromSite: ' + link
+          );
+          resp = await getSiteData(link, tokens[j].properties.Name.title[0].text.content, tokens[j].properties.Id.number);
+
+          // if (repos && Array.isArray(repos)) {
+          //   await LoggerInstance.makeReport(repos);
+          //   await asyncForEach(repos, async (repo, index) => {
+          //     await LoggerInstance.logInfo(
+          //       `scrapSiteData TRYING TO SAVE REPO TO NOTION ${repo.name}: \n\n` +
+          //         JSON.stringify(repo),
+          //       false
+          //     );
+          //     await addRepsoitoryToNotion(tokens[j], repo);
+          //   });
+          // } else {
+          //   await LoggerInstance.logInfo(
+          //     `scrapSiteData NO REPOS FOR LINK: ${link}`
+          //   );
+          // }
+        });
+      }
+      resp.browser.close();
+    }
+    await LoggerInstance.logInfo('scrapSiteData DONE');
+  } catch (error) {
+    console.log(error);
+    await LoggerInstance.logError(
+      `scrapSiteData \n\n ${error.message} \n\n ${error.stack}`
+    );
   }
 }
+
 
 // ***************** RUN ***************
 
 // await migrateFromCoinmarketCap();
-await downloadProjectRepositories();
-//await generateJsonExtentationFile();
+// await downloadProjectRepositories();
+// await generateJsonExtentationFile();
+// await scrapSiteData()
+
+
+
+
+
 
 // **************** TESTS *****************
 
